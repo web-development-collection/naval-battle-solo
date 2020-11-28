de1b96e19618cb8abd2f7f94259d1143543108af
ingoandelhofs <ingoandelhofs@gmail.com>
2020-11-28 12:29:16 +0100

    :tada: Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class App extends Component<any, any> {
+
+
+  public render() {
+    return <div className="App">
+      <h1>Naval Battle</h1>
+    </div>;
+  }
+}
+
+export default App;
